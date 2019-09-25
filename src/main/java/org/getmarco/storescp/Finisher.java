b90daf35549b26fedba8c8140cbeb07b9500746b
Marco Chiesa <getmarcochiesa@gmail.com>
2019-09-24 20:55:50 -0500

    register storage SCP, prep work area on startup

---
+++ src/main/java/org/getmarco/storescp/Finisher.java
@@ -0,0 +1,26 @@
+package org.getmarco.storescp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Finisher {
+    private static final Logger LOG = LoggerFactory.getLogger(Finisher.class);
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    public void finish() {
+        finish(0);
+    }
+
+    public void finish(int returnCode) {
+        LOG.info("shutting down application");
+        int n = SpringApplication.exit(applicationContext, () -> returnCode);
+        System.exit(n);
+    }
+}
--- src/main/java/org/getmarco/storescp/Starter.java
+++ src/main/java/org/getmarco/storescp/Starter.java
@@ -11,8 +11,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.util.StringUtils;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -24,6 +29,11 @@ public class Starter implements CommandLineRunner {
 
     @Autowired
     private Config config;
+    @Autowired
+    private Finisher finisher;
+    @Autowired
+    private CStoreSCP storeSCP;
+
     private Device device;
     private ApplicationEntity ae;
     private Connection conn;
@@ -64,11 +74,44 @@ public class Starter implements CommandLineRunner {
         device.setExecutor(executorService);
         device.bindConnections();
         LOG.info("device started");
+
+        String storage = config.getStorageDir();
+        LOG.info("checking storage directory: {}", storage);
+        if (storage == null)
+            fail("null storage directory");
+        Path storageDir = Paths.get(storage);
+        if (config.isClearStorageDirectoryOnStart()) {
+            try {
+                FileSystemUtils.deleteRecursively(storageDir);
+            } catch (IOException ex) {
+                LOG.error("unable to clear storage directory: " + storageDir, ex);
+                finisher.finish(1);
+            }
+        }
+        Files.createDirectories(storageDir);
+        if (!Files.exists(storageDir))
+            fail("unable to create storage directory: " + storageDir);
+
+        Path incomingDir = storageDir.resolve(Config.INCOMING_DIR);
+        Files.createDirectory(incomingDir);
+        if (!Files.exists(incomingDir))
+            fail("unable to create incoming directory: " + incomingDir);
+
+        Path zipDir = storageDir.resolve(Config.ZIP_DIR);
+        Files.createDirectory(zipDir);
+        if (!Files.exists(zipDir))
+            fail("unable to create incoming directory: " + zipDir);
     }
 
     private DicomServiceRegistry createServiceRegistry() {
         DicomServiceRegistry serviceRegistry = new DicomServiceRegistry();
         serviceRegistry.addDicomService(new BasicCEchoSCP());
+        serviceRegistry.addDicomService(storeSCP);
         return serviceRegistry;
+    }
+
+    private void fail(String message) {
+        LOG.error(message);
+        finisher.finish(1);
     }
 }
