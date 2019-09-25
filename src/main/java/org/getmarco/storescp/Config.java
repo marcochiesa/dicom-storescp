849a6342da0abee74b63c4dc38b389d781f91c82
Marco Chiesa <getmarcochiesa@gmail.com>
2019-09-24 20:43:50 -0500

    add config for study wait time and study processing work area

--- src/main/java/org/getmarco/storescp/Config.java
+++ src/main/java/org/getmarco/storescp/Config.java
@@ -6,9 +6,16 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties(prefix = "storescp")
 public class Config {
+    public static final String PART_EXT = ".part";
+    public static final String DCM_EXT = ".dcm";
+    public static final String INCOMING_DIR = "incoming";
+    public static final String ZIP_DIR = "zip";
 
     private String deviceName;
+    private String storageDir;
     private int port;
+    private int studyWaitTime;
+    private boolean clearStorageDirectoryOnStart;
 
     public String getDeviceName() {
         return deviceName;
@@ -18,11 +25,35 @@ public class Config {
         this.deviceName = deviceName;
     }
 
+    public String getStorageDir() {
+        return storageDir;
+    }
+
+    public void setStorageDir(String storageDir) {
+        this.storageDir = storageDir;
+    }
+
     public int getPort() {
         return port;
     }
 
     public void setPort(int port) {
         this.port = port;
+    }
+
+    public int getStudyWaitTime() {
+        return studyWaitTime;
+    }
+
+    public void setStudyWaitTime(int studyWaitTime) {
+        this.studyWaitTime = studyWaitTime;
+    }
+
+    public boolean isClearStorageDirectoryOnStart() {
+        return clearStorageDirectoryOnStart;
+    }
+
+    public void setClearStorageDirectoryOnStart(boolean clearStorageDirectoryOnStart) {
+        this.clearStorageDirectoryOnStart = clearStorageDirectoryOnStart;
     }
 }
