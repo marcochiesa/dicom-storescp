2176fd9280e19db065ee8f2c124f492d8f13608f
Marco Chiesa <getmarcochiesa@gmail.com>
2019-09-30 02:27:33 -0500

    fix output of incoming endpoint

--- src/main/java/org/getmarco/storescp/IncomingEndpoint.java
+++ src/main/java/org/getmarco/storescp/IncomingEndpoint.java
@@ -10,8 +10,10 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.stream.Stream;
@@ -24,54 +26,60 @@ public class IncomingEndpoint {
     public static final String STUDY_UID_KEY = "studyUid";
     public static final String FILE_COUNT_KEY = "fileCount";
     public static final String ERROR_KEY = "error";
-    public static final String CAUSE_KEY = "cause";
 
     @Autowired
     private Config config;
 
     @ReadOperation
-    public Map<String, Map<String, String>> incoming() {
-        Map<String, Map<String, String>> map = new TreeMap<>();
+    public Map<String, ? extends Object> incoming() {
+        Map<String, List<Map<String, String>>> map = new TreeMap<>();
         Path storageDir = config.getStorageDirPath();
         try (Stream<Path> stream = Files.list(storageDir)) {
             stream.filter(config::isCalledAETDir).forEach( calledAETDir -> {
                 String calledAET = calledAETDir.getFileName().toString();
-                Map<String, String> studies = checkCalledAETDir(calledAETDir);
+                List<Map<String, String>> studies = checkCalledAETDir(calledAETDir);
                 if (!studies.isEmpty())
                     map.put(calledAET, studies);
             });
         } catch(IOException e) {
             LOG.error("unable to process storage directory: " + storageDir, e);
-            Map <String, String> cause = Collections.<String, String>singletonMap(CAUSE_KEY, e.getMessage());
-            return Collections.<String, Map<String, String>>singletonMap(ERROR_KEY, cause);
+            return Collections.singletonMap(ERROR_KEY, e.getMessage());
         }
         return map;
     }
 
-    private Map<String, String> checkCalledAETDir(Path calledAETDir) {
-        Map<String, String> map = new HashMap<>();
+    private List<Map<String, String>> checkCalledAETDir(Path calledAETDir) {
+        List<Map<String, String>> list = new ArrayList<>();
         try (Stream<Path> callingAETDirs = Files.list(calledAETDir)) {
-            callingAETDirs.forEach(callingAETDir -> {
-                try (Stream<Path> studyDirs = Files.list(callingAETDir)) {
-                    studyDirs.forEach(studyDir -> {
-                        String studyUid = studyDir.getFileName().toString();
-                        long fileCount = checkStudyDir(studyDir);
-                        if (fileCount > 0) {
-                            map.put(STUDY_UID_KEY, studyUid);
-                            map.put(FILE_COUNT_KEY, String.valueOf(fileCount));
-                        }
-                    });
-                } catch(IOException e) {
-                    throw new RuntimeException(e);
-                }
-            });
+            callingAETDirs.forEach(callingAETDir -> {list.addAll(checkCallingAETDir(callingAETDir));});
         } catch(IOException e) {
             throw new RuntimeException(e);
         }
+        return list;
+    }
+
+    private List<Map<String, String>> checkCallingAETDir(Path callingAETDir) {
+        List<Map<String, String>> list = new ArrayList<>();
+        try (Stream<Path> studyDirs = Files.list(callingAETDir)) {
+            studyDirs.forEach(studyDir -> {list.add(checkStudyDir(studyDir));});
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
+
+    private Map<String, String> checkStudyDir(Path studyDir) {
+        Map<String, String> map = new HashMap<>();
+        String studyUid = studyDir.getFileName().toString();
+        long fileCount = countStudyFiles(studyDir);
+        if (fileCount > 0) {
+            map.put(STUDY_UID_KEY, studyUid);
+            map.put(FILE_COUNT_KEY, String.valueOf(fileCount));
+        }
         return map;
     }
 
-    private long checkStudyDir(Path studyDir) {
+    private long countStudyFiles(Path studyDir) {
         try (Stream<Path> studyFiles = Files.list(studyDir)) {
             return studyFiles.count();
         } catch(IOException e) {
